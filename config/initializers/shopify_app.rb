22382df491bdfafac08cab8b5841b7b069162843
Singleshot <singlesh0tdiocampo@gmail.com>
2018-06-05 13:19:02 +0000

    Installed Shopify App

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "c0f5f35c780825f601e7df96c7fc4bd8"
+  config.secret = "3a3e05d78e863398a82d6c7a48fe3f1a"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
